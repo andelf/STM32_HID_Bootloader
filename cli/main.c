4c0fba5335431aa8701308e23ab66939e59cdf0f
Andelf <andelf@gmail.com>
2020-11-25 17:55:25 +0800

    *: reverse WeAct protocol

--- cli/main.c
+++ cli/main.c
@@ -33,9 +33,10 @@
 #define HID_TX_SIZE    65
 #define HID_RX_SIZE     9
 
-#define VID           0x1209
-#define PID           0xBEBA
-#define FIRMWARE_VER  0x0300
+#define VID           0x0483
+#define PID           0x572A
+// 0x0300
+#define FIRMWARE_VER  0x0200
 
 int serial_init(char *argument, uint8_t __timer);
 
@@ -63,8 +64,9 @@ int main(int argc, char *argv[]) {
   uint8_t page_data[SECTOR_SIZE];
   uint8_t hid_tx_buf[HID_TX_SIZE];
   uint8_t hid_rx_buf[HID_RX_SIZE];
-  uint8_t CMD_RESET_PAGES[8] = {'B','T','L','D','C','M','D', 0x00};
-  uint8_t CMD_REBOOT_MCU[8] = {'B','T','L','D','C','M','D', 0x01};
+  uint8_t CMD_DEVICE_INFO[8] = {'W','e','A','c','t',':', 0x02, 0x00};
+  uint8_t CMD_RESET_PAGES[8] = {'W','e','A','c','t',':', 0x00, 0x00};
+  uint8_t CMD_REBOOT_MCU[8] = {'W','e','A','c','t',':', 0x01, 0x00};
   hid_device *handle = NULL;
   size_t read_bytes;
   FILE *firmware_file = NULL;
@@ -72,47 +74,47 @@ int main(int argc, char *argv[]) {
   uint32_t n_bytes = 0;
   int i;
   setbuf(stdout, NULL);
-  uint8_t _timer = 0;
-  
+
   printf("\n+-----------------------------------------------------------------------+\n");
   printf  ("|         HID-Flash v2.2.1 - STM32 HID Bootloader Flash Tool            |\n");
   printf  ("|     (c)      2018 - Bruno Freitas       http://www.brunofreitas.com   |\n");
   printf  ("|     (c) 2018-2019 - Vassilis Serasidis  https://www.serasidis.gr      |\n");
   printf  ("|   Customized for STM32duino ecosystem   https://www.stm32duino.com    |\n");
   printf  ("+-----------------------------------------------------------------------+\n\n");
-  
-  if(argc < 3) {
-    printf("Usage: hid-flash <bin_firmware_file> <comport> <delay (optional)>\n");
+
+  if(argc != 2) {
+    printf("Usage: hid-flash <bin_firmware_file>\n");
     return 1;
-  }else if(argc == 4){
-    _timer = atol(argv[3]);
   }
-  
+
   firmware_file = fopen(argv[1], "rb");
   if(!firmware_file) {
     printf("> Error opening firmware file: %s\n", argv[1]);
     return error;
   }
-  
+
+/*
   if(serial_init(argv[2], _timer) == 0){ //Setting up Serial port
     RS232_CloseComport();
   }else{
     printf("> Unable to open the [%s]\n",argv[2]);
   }
-  
+  */
+
   hid_init();
-  
+
   printf("> Searching for [%04X:%04X] device...\n",VID,PID);
-  
+
   struct hid_device_info *devs, *cur_dev;
   uint8_t valid_hid_devices = 0;
-  
+
   for(i=0;i<10;i++){ //Try up to 10 times to open the HID device.
     devs = hid_enumerate(VID, PID);
     cur_dev = devs;
     while (cur_dev) { //Search for valid HID Bootloader USB devices
       if((cur_dev->vendor_id == VID)&&(cur_dev->product_id = PID)){
         valid_hid_devices++;
+        printf("Release No: %d\n", cur_dev->release_number);
         if(cur_dev->release_number < FIRMWARE_VER){ //The STM32 board has firmware lower than 3.00
           printf("\nError - Please update the firmware to the latest version (v3.00+)");
           goto exit;
@@ -129,18 +131,30 @@ int main(int argc, char *argv[]) {
     printf("\nError - [%04X:%04X] device is not found :(",VID,PID);
     error = 1;
     goto exit;
-  } 
-  
+  }
+
   handle = hid_open(VID, PID, NULL);
-  
+
   if (i == 10 && handle != NULL) {
     printf("\n> Unable to open the [%04X:%04X] device.\n",VID,PID);
     error = 1;
     goto exit;
   }
- 
+
   printf("\n> [%04X:%04X] device is found !\n",VID,PID);
-  
+
+  memset(hid_tx_buf, 0, sizeof(hid_tx_buf)); //Fill the hid_tx_buf with zeros.
+  memcpy(&hid_tx_buf[1], CMD_DEVICE_INFO, sizeof(CMD_DEVICE_INFO));
+  printf("> Sending <read FW ver> command...\n");
+  if(!usb_write(handle, hid_tx_buf, HID_TX_SIZE)) {
+    printf("> Error while sending <reset pages> command.\n");
+    error = 1;
+    goto exit;
+  }
+  usleep(500);
+  hid_read(handle, hid_tx_buf, HID_TX_SIZE); // reuse
+  printf("> %s\n", hid_tx_buf);
+
   // Send RESET PAGES command to put HID bootloader in initial stage...
   memset(hid_tx_buf, 0, sizeof(hid_tx_buf)); //Fill the hid_tx_buf with zeros.
   memcpy(&hid_tx_buf[1], CMD_RESET_PAGES, sizeof(CMD_RESET_PAGES));
@@ -169,7 +183,7 @@ int main(int argc, char *argv[]) {
       if((i % 1024) == 0){
         printf(".");
       }
-      
+
       // Flash is unavailable when writing to it, so USB interrupt may fail here
       if(!usb_write(handle, hid_tx_buf, HID_TX_SIZE)) {
         printf("> Error while flashing firmware data.\n");
@@ -179,20 +193,20 @@ int main(int argc, char *argv[]) {
       n_bytes += (HID_TX_SIZE - 1);
       usleep(500);
     }
-    
+
     printf(" %d Bytes\n", n_bytes);
 
     do{
       hid_read(handle, hid_rx_buf, 9);
       usleep(500);
-    }while(hid_rx_buf[7] != 0x02);
-    
+    }while(hid_rx_buf[6] != 0x03); // WeAct:\x03
+
     memset(page_data, 0, sizeof(page_data));
     read_bytes = fread(page_data, 1, sizeof(page_data), firmware_file);
   }
 
   printf("\n> Done!\n");
-  
+
   // Send CMD_REBOOT_MCU command to reboot the microcontroller...
   memset(hid_tx_buf, 0, sizeof(hid_tx_buf));
   memcpy(&hid_tx_buf[1], CMD_REBOOT_MCU, sizeof(CMD_REBOOT_MCU));
@@ -203,7 +217,7 @@ int main(int argc, char *argv[]) {
   if(!usb_write(handle, hid_tx_buf, HID_TX_SIZE)) {
     printf("> Error while sending <reboot mcu> command.\n");
   }
-  
+
 exit:
   if(handle) {
     hid_close(handle);
@@ -214,7 +228,8 @@ exit:
   if(firmware_file) {
     fclose(firmware_file);
   }
-  
+
+ /*
   printf("> Searching for [%s] ...\n",argv[2]);
 
   for(int i=0;i<5;i++){
@@ -224,12 +239,13 @@ exit:
     }
     sleep(1);
   }
-  
+
   if(i==5){
     printf("> Comport is not found\n");
   }
+  */
   printf("> Finish\n");
-  
+
   return error;
 }
 
@@ -240,7 +256,7 @@ int serial_init(char *argument, uint8_t __timer) {
     return(1);
   }
   printf("> Toggling DTR...\n");
-  
+
   RS232_disableRTS();
   RS232_enableDTR();
   usleep(200000L);
@@ -253,7 +269,7 @@ int serial_init(char *argument, uint8_t __timer) {
   RS232_send_magic();
   usleep(200000L);
   RS232_CloseComport();
-  
+
   //printf("A %i\n",__timer);
   if (__timer > 0) {
     usleep(__timer);
